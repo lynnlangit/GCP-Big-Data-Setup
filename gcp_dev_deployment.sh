27d696deb4f87066bcc729bfd2024b0b9c61244d
Lynn Langit <lynnlangit@live.com>
2016-01-13 09:28:43 -0800

    update names

--- gcp_dev_deployment.sh
+++ gcp_dev_deployment.sh
@@ -6,13 +6,13 @@ gcloud auth login $ACCOUNT --brief              # authenticate to GCP from your 
                                                 # will not launch if $ACCOUNT already has credentials
 # ------------------- WORKING WITH YOUR DEPLOYMENT -----------------------
 # 1. DEPLOY IT
-gcloud deployment-manager deployments create my-first-deployment --config vm.yaml
+gcloud deployment-manager deployments create dev-env-deployment --config dev-env-deployment.yaml
 
 # 2a. SEE STATUS - you can also view on the GCP console
-gcloud deployment-manager deployments describe my-first-deployment
+gcloud deployment-manager deployments describe dev-env-deployment
 
 # 2b. SEE RESOURCES
-gcloud deployment-manager resources list --deployment my-first-deployment
+gcloud deployment-manager resources list --deployment dev-env-deployment
 
 # 3. DELETE IT
-gcloud deployment-manager deployments delete my-first-deployment
+gcloud deployment-manager deployments delete dev-env-deployment
